2a47122dffe65843118e695a7d52ea839c3a3808
Goksel Yesiller <gokselyesiller@gmail.com>
2022-02-21 00:33:57 +0300

    Update index.js

--- ReactJs/Task1/index.js
+++ ReactJs/Task1/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-async function getData(number) {
-    const { data: user } = await axios(`https://jsonplaceholder.typicode.com/users/${number}`);
-    const { data: posts } = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${number}`);
+async function getData(id) {
+    const { data: user } = await axios(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const { data: posts } = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
 
     return {user, posts}
 }
